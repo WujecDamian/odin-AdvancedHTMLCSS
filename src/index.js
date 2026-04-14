fc6074409099df302e566fdd659f1ef4e9e8947a
WujecDamian <wujecdamian12@gmail.com>
2026-04-14 17:37:51 +0200

    feat: add animation listeners and listener function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
 //put inside src/index.js
-import './styles.css'
+import "./styles.css";
+
+const watchMeElement = document.querySelector("#watch-me");
+
+watchMeElement.addEventListener("animationstart", listener);
+watchMeElement.addEventListener("animationend", listener);
+watchMeElement.addEventListener("animationiteration", listener);
+
+watchMeElement.className = "slide-in";
+
+function listener(event) {
+  const span = document.createElement("span");
+  switch (event.type) {
+    case "animationstart":
+      span.textContent = `Started: elapsed time is ${event.elapsedTime}!`;
+      break;
+    case "animationend":
+      span.textContent = `Ended: elapsed time is ${event.elapsedTime}!`;
+      break;
+    case "animationiteration":
+      span.textContent = `New loop started at time ${event.elapsedTime}!`;
+      break;
+  }
+  document.querySelector("#output").appendChild(span);
+}
